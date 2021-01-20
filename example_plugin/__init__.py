4782dd8e694d67697dd420d5fb2b20a871462acb
Milo Weinberg <38477514+Iapetus-11@users.noreply.github.com>
2021-01-19 23:59:04 -0500

    Update example_handle_handshake() for refactor

--- example_plugin/__init__.py
+++ example_plugin/__init__.py
@@ -3,10 +3,10 @@ from pymine.api.packet import handle_packet
 
 
 @handle_packet('handshaking', 0x00)
-async def example_handle_handshake(r, w, packet, remote):
+async def example_handle_handshake(stream, packet):
     print('Hello this is the example packet handler speaking how may I take your order sir')
 
-    return True, r, w
+    return True, stream
 
 
 @on_server_ready
