eda0b86b7ec33e9965245a7663b80946b9b08819
Milo Weinberg <iapetus011@gmail.com>
2021-02-11 21:11:22 -0500

    Update example methods to be clearer about where they're from

--- example_plugin/__init__.py
+++ example_plugin/__init__.py
@@ -3,25 +3,25 @@ from pymine.server import server
 
 @server.api.events.on_packet("handshaking", 0x00)
 async def example_handle_handshake(stream, packet):
-    server.logger.debug("Handshake packet received (example plugin speaking!)")
+    server.logger.debug("EXAMPLE PLUGIN: Handshake packet received!")
 
     return True, stream
 
 
 @server.api.events.on_server_ready
 async def on_server_ready():
-    server.logger.info("AYYY SERVER DO BE READY THO NGL?")
+    server.logger.info("EXAMPLE PLUGIN: AYYY SERVER DO BE READY THO NGL?")
 
-    
+
 @server.api.events.on_server_stop
 async def on_server_stop():
-    server.logger.info("bish you better start this server back up")
+    server.logger.info("EXAMPLE PLUGIN: bish you better start this server back up")
 
 
 async def setup(server, plugin_yml):
-    server.logger.info("setup coroutine called for the example plugin!")
+    server.logger.info("EXAMPLE PLUGIN: setup coroutine called!")
     server.logger.debug(str(plugin_yml))
 
 
 async def teardown(server):
-    server.logger.info("teardown coroutine called for the example plugin!")
+    server.logger.info("EXAMPLE PLUGIN: teardown coroutine!")
