87ca915b4d1c05291c210dac05c601eb28b2b66b
Milo Weinberg <iapetus011@gmail.com>
2021-03-08 09:01:31 -0500

    comment out old plugin structure

--- example_plugin/__init__.py
+++ example_plugin/__init__.py
@@ -1,26 +1,26 @@
-from pymine.server import server
-
-
-@server.api.events.on_packet("handshaking", 0x00)
-async def example_handle_handshake(stream, packet):
-    server.console.debug("EXAMPLE PLUGIN: Handshake packet received!")
-
-    return True, stream
-
-
-@server.api.events.on_server_ready
-async def on_server_ready():
-    server.console.info("EXAMPLE PLUGIN: AYYY SERVER DO BE READY THO NGL?")
-
-
-@server.api.events.on_server_stop
-async def on_server_stop():
-    server.console.info("EXAMPLE PLUGIN: bish you better start this server back up")
-
-
-async def setup(server, plugin_yml):
-    server.console.info("EXAMPLE PLUGIN: setup coroutine called!")
-
-
-async def teardown(server):
-    server.console.info("EXAMPLE PLUGIN: teardown coroutine!")
+# from pymine.server import server
+#
+#
+# @server.api.events.on_packet("handshaking", 0x00)
+# async def example_handle_handshake(stream, packet):
+#     server.console.debug("EXAMPLE PLUGIN: Handshake packet received!")
+#
+#     return True, stream
+#
+#
+# @server.api.events.on_server_ready
+# async def on_server_ready():
+#     server.console.info("EXAMPLE PLUGIN: AYYY SERVER DO BE READY THO NGL?")
+#
+#
+# @server.api.events.on_server_stop
+# async def on_server_stop():
+#     server.console.info("EXAMPLE PLUGIN: bish you better start this server back up")
+#
+#
+# async def setup(server, plugin_yml):
+#     server.console.info("EXAMPLE PLUGIN: setup coroutine called!")
+#
+#
+# async def teardown(server):
+#     server.console.info("EXAMPLE PLUGIN: teardown coroutine!")
