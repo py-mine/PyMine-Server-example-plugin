14090a10930fb85a6dbaea9499698885a72fe4f7
Milo Weinberg <iapetus011@gmail.com>
2021-03-08 10:36:17 -0500

    update import

--- example_plugin/__init__.py
+++ example_plugin/__init__.py
@@ -1,5 +1,5 @@
 from pymine.api.abc import AbstractPlugin
-from pymine.server import server
+from pymine import add_plugin
 
 
 class MyExamplePlugin(AbstractPlugin):
@@ -24,7 +24,7 @@ class MyExamplePlugin(AbstractPlugin):
 
 
 async def setup(server, plugin_yml):
-    server.api.add_plugin(MyExamplePlugin(server, plugin_yml))
+    add_plugin(MyExamplePlugin(server, plugin_yml))
 
 
 # from pymine.server import server
