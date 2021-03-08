5a34cd6f05a9a7a47f18f652217cb47e5c6959ef
Milo Weinberg <iapetus011@gmail.com>
2021-03-08 09:06:36 -0500

    add basic new plugin structure

--- example_plugin/__init__.py
+++ example_plugin/__init__.py
@@ -1,3 +1,29 @@
+from pymine.api.abc import AbstractPlugin
+from pymine.server import server
+
+class MyExamplePlugin(AbstractPlugin):
+    def __init__(self, server):
+        self.server = server
+
+    @server.api.register.on_packet("handshaking", 0x00)
+    async def my_packet_handler(stream, placket):
+        server.console.debug("EXAMPLE PLUGIN: Handshake packet received!")
+
+    @server.api.register.on_server_start
+    async def my_server_start_handler():
+        server.console.debug("EXAMPLE PLUGIN: Ayyy server do be ready tho ngl")
+
+    @server.api.register.on_server_stop
+    async def my_server_stop_handler():
+        server.console.debug("EXAMPLE PLUGIN: bishhh you better be starting this server right back up or else")
+
+
+def setup(server):
+    server.api.add_plugin(MyExamplePlugin(server))
+
+
+
+
 # from pymine.server import server
 #
 #
