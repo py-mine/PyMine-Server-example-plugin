17c29f8bbc03da5355cc135e3fa7f45ef44f7d84
Milo Weinberg <38477514+Iapetus-11@users.noreply.github.com>
2021-03-07 20:34:20 -0500

    Update calls to server.console

--- example_plugin/__init__.py
+++ example_plugin/__init__.py
@@ -3,25 +3,24 @@ from pymine.server import server
 
 @server.api.events.on_packet("handshaking", 0x00)
 async def example_handle_handshake(stream, packet):
-    server.logger.debug("EXAMPLE PLUGIN: Handshake packet received!")
+    server.console.debug("EXAMPLE PLUGIN: Handshake packet received!")
 
     return True, stream
 
 
 @server.api.events.on_server_ready
 async def on_server_ready():
-    server.logger.info("EXAMPLE PLUGIN: AYYY SERVER DO BE READY THO NGL?")
+    server.console.info("EXAMPLE PLUGIN: AYYY SERVER DO BE READY THO NGL?")
 
 
 @server.api.events.on_server_stop
 async def on_server_stop():
-    server.logger.info("EXAMPLE PLUGIN: bish you better start this server back up")
+    server.console.info("EXAMPLE PLUGIN: bish you better start this server back up")
 
 
 async def setup(server, plugin_yml):
-    server.logger.info("EXAMPLE PLUGIN: setup coroutine called!")
-    server.logger.debug(str(plugin_yml))
+    server.console.info("EXAMPLE PLUGIN: setup coroutine called!")
 
 
 async def teardown(server):
-    server.logger.info("EXAMPLE PLUGIN: teardown coroutine!")
+    server.console.info("EXAMPLE PLUGIN: teardown coroutine!")
